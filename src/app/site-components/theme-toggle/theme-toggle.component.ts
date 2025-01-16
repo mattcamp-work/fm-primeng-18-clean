eac7e31901891d7d016ef309c8fa73011a4e7119
mattcamp-work <mattcamp.main@gmail.com>
2025-01-16 09:43:38 -0500

    add theme toggle

--- src/app/site-components/theme-toggle/theme-toggle.component.ts
+++ src/app/site-components/theme-toggle/theme-toggle.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnDestroy } from '@angular/core';
+
 
 @Component({
   selector: 'app-theme-toggle',
   templateUrl: './theme-toggle.component.html',
   styleUrl: './theme-toggle.component.scss'
 })
-export class ThemeToggleComponent {
 
+export class ThemeToggleComponent implements OnDestroy {
+  private readonly className = 'fm-dark-mode';
+
+  constructor(private renderer: Renderer2) {}
+
+  toggleClass(): void {
+    const htmlElement = document.documentElement; // <html>
+    if (htmlElement.classList.contains(this.className)) {
+      this.renderer.removeClass(htmlElement, this.className);
+    } else {
+      this.renderer.addClass(htmlElement, this.className);
+    }
+  }
+
+  addClass(): void {
+    const htmlElement = document.documentElement;
+    this.renderer.addClass(htmlElement, this.className);
+  }
+
+  removeClass(): void {
+    const htmlElement = document.documentElement;
+    this.renderer.removeClass(htmlElement, this.className);
+  }
+
+  ngOnDestroy(): void {
+    // Clean up by ensuring the class is removed when the component is destroyed
+    this.removeClass();
+  }
 }

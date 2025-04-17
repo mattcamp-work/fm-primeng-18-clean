67419a9caa065a24dff9f68a80bb382b3ae48838
mattcamp-work <mattcamp.main@gmail.com>
2025-04-17 10:27:46 -0400

    update to fix angular encapsulation

--- src/app/comp-views/token-export/token-export.component.ts
+++ src/app/comp-views/token-export/token-export.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as fmColorLibrary from '~/fm-theme/fm-color-tokens';
 import _ from 'lodash';
